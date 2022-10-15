a6ccf38fe3d83dec5a17686755357f5c69d406f1
Filipe Pina <fopina@gmail.com>
2022-10-15 23:22:24 +0100

    browserproxy: small changes

--- browserproxy/main.py
+++ browserproxy/main.py
@@ -1,7 +1,14 @@
-from selenium import webdriver
-from pathlib import Path
-from selenium.webdriver.common.by import By
+import argparse
 import json
+import logging
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from pathlib import Path
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
+logger = logging.getLogger(__name__)
 
 
 class MyDriver(webdriver.Chrome):
@@ -26,55 +33,67 @@ class MyDriver(webdriver.Chrome):
             {"userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0"},
         )
 
-if __name__ == '__main__':
-    import logging
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
-    logger = logging.getLogger(__name__)
 
-    logger.info("logging in")
-    driver = MyDriver(docker=True)
+class MyServer(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = json.dumps(self.rfile.read(content_len).decode())
+        try:
+            data = self.server.driver.execute_script(
+                f'''
+                const r = await fetch(
+                    "https://api.snailtrail.art/graphql/",
+                    {{
+                        method: 'POST',
+                        headers: {{
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        }},
+                    body: {post_body}
+                    }}
+                );
+                return [r.status, await r.text()];
+                '''
+            )
+            self.send_response(data[0])
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(bytes(data[1], "utf-8"))
+        except Exception as e:
+            self.send_response(500)
+            self.send_header("Content-type", "text/plan")
+            self.end_headers()
+            self.wfile.write(bytes(str(e), "utf-8"))
+            raise
+
+
+def build_parser():
+    p = argparse.ArgumentParser()
+    p.add_argument('--endpoint', type=str, default='https://api.snailtrail.art/graphql/', help='GraphQL endpoint')
+    p.add_argument(
+        '--show',
+        action='store_true',
+        help='Show browser',
+    )
+    p.add_argument('-b', '--bind-address', default='127.0.0.1', help='Server listening interface')
+    p.add_argument('-p', '--port', default=8888, help='Server listening port')
+    return p
+
+
+def main(argv=None):
+    args = build_parser().parse_args(argv)
+    logger.info("launching browser")
+    driver = MyDriver(docker=not args.show)
     driver.implicitly_wait(20)
 
+    driver.get(args.endpoint)
+    # random element to wait for page load
+    driver.find_element(By.TAG_NAME, "textarea")
+    logger.info('starting httpd')
 
-    driver.get("https://api.snailtrail.art/graphql/")
-    el = driver.find_element(By.TAG_NAME, "textarea")
-    logger.info('ready')
-    from http.server import BaseHTTPRequestHandler, HTTPServer
-
-    class MyServer(BaseHTTPRequestHandler):
-        def do_POST(self):
-            content_len = int(self.headers.get('content-length', 0))
-            post_body = json.dumps(self.rfile.read(content_len).decode())
-            try:
-                data = driver.execute_script(
-                    f'''
-                    const r = await fetch(
-                        "https://api.snailtrail.art/graphql/",
-                        {{
-                            method: 'POST',
-                            headers: {{
-                                'Accept': 'application/json',
-                                'Content-Type': 'application/json'
-                            }},
-                        body: {post_body}
-                        }}
-                    );
-                    return [r.status, await r.text()];
-                    '''
-                )
-                self.send_response(data[0])
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(bytes(data[1], "utf-8"))
-            except Exception as e:
-                self.send_response(500)
-                self.send_header("Content-type", "text/plan")
-                self.end_headers()
-                self.wfile.write(bytes(str(e), "utf-8"))
-                raise
-
-    webServer = HTTPServer(('', 8888), MyServer)
-    print("Server started http://%s:%s" % ('', 8888))
+    webServer = HTTPServer((args.bind_address, args.port), MyServer)
+    webServer.driver = driver
+    print(f'Server started http://{args.bind_address}:{args.port}')
 
     try:
         webServer.serve_forever()
@@ -83,3 +102,7 @@ if __name__ == '__main__':
 
     webServer.server_close()
     print("Server stopped.")
+
+
+if __name__ == '__main__':
+    main()
