1cc54017f4751bacdbd2963b6bbcc90358607745
Filipe Pina <fopina@gmail.com>
2022-11-02 23:40:38 +0000

    browserproxy: new flag

--- browserproxy/main.py
+++ browserproxy/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import argparse
 import json
 import logging
@@ -22,6 +24,7 @@ class MyDriver(webdriver.Chrome):
             options.headless = True
             options.add_argument("--disable-gpu")
             options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
             options.add_argument("start-maximized")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option("useAutomationExtension", False)
