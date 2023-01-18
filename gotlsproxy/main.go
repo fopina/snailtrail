b4bef24de2644a4e79fdbc1f76ac5b8170c940ec
Filipe Pina <fopina@gmail.com>
2023-01-18 00:34:29 +0000

    gotlsproxy: added cycletls (success)

--- gotlsproxy/main.go
+++ gotlsproxy/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"io"
 	"net/http"
-	"time"
+
+	"github.com/Danny-Dasilva/CycleTLS/cycletls"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
@@ -16,25 +17,36 @@ func hello(w http.ResponseWriter, req *http.Request) {
 			request.Header.Add(name, h)
 		}
 	}
-	client := http.Client{
-		Timeout: time.Duration(5 * time.Second),
-	}
-	response, err := client.Do(request)
+	client := cycletls.Init()
+
+	body, err := io.ReadAll(request.Body)
 	if err != nil {
 		panic(err)
-	}
-	data, err := io.ReadAll(response.Body)
-	if err != nil {
-		panic(err)
-	}
-	w.WriteHeader(response.StatusCode)
-	for name, headers := range response.Header {
-		for _, h := range headers {
-			w.Header().Add(name, h)
-		}
 	}
 
-	w.Write(data)
+	response, err := client.Do("https://api.snailtrail.art/graphql/", cycletls.Options{
+		Body:      string(body),
+		Ja3:       "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0",
+		UserAgent: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0",
+		Headers: map[string]string{
+			"Accept":       "application/json",
+			"Content-Type": "application/json",
+		},
+		Timeout: 5,
+	}, "POST")
+	if err != nil {
+		panic(err)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(response.Status)
+	for name, h := range response.Headers {
+		w.Header().Add(name, h)
+	}
+
+	w.Write([]byte(response.Body))
 }
 
 func main() {
