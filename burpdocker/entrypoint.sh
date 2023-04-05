8cc8303702bcab15fab99d7004c4c854f26be974
Filipe Pina <fopina@gmail.com>
2023-04-05 14:33:42 +0100

    cleanup

--- burpdocker/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# burp CE won't allow changing to LISTEN ON ALL INTERFACES via CLI - use socat to proxy
-socat TCP4-LISTEN:8081,fork,reuseaddr TCP4:127.0.0.1:8080 &
-# answer "yes" to terms of agreement
-yes | java -Djava.awt.headless=true -jar burp.jar burp.StartBurp
