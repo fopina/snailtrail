5d40163e737d3201b7f68ab331716f4c52383674
Filipe Pina <fopina@gmail.com>
2023-05-24 06:30:13 +0100

    update_abi - finally useable

--- update_abi.py
+++ update_abi.py
@@ -3,13 +3,26 @@
 import requests
 import re
 import json5
+from pathlib import Path
+import subprocess
 
 EXPECTED_CONTRACTS = 12
+CONTRACT_DIR = Path(__file__).absolute().parent / 'snail' / 'contracts'
 
 
 def assert_equal(expected, got):
     if expected != got:
         raise AssertionError(f'Expected {expected}, got {got}')
+
+
+def assert_in(container, member):
+    if member not in container:
+        raise AssertionError(f'Expected {member} to be in {container}')
+
+
+def camel_to_snake(name):
+    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
 
 
 class Parser:
@@ -29,7 +42,15 @@ class Parser:
 
     def extract_abi(self):
         first_l = None
-        for l in re.findall(r'(\bextends \w+?\.ContractFactory)\b', self.script_data):
+        for l in re.findall(r'(\bextends (\w+?)\.ContractFactory)\b', self.script_data):
+            if first_l is None:
+                first_l = l
+            else:
+                assert_equal(first_l, l)
+
+        contract_class = first_l[1]
+        first_l = None
+        for l in re.findall(rf'(\bnew {contract_class}\.Contract)\b', self.script_data):
             if first_l is None:
                 first_l = l
             else:
@@ -79,13 +100,52 @@ class Parser:
 
             abis.append((var_factory, var_abi, abi_definition))
 
-        # FIXME: find missing contract
-        assert_equal(EXPECTED_CONTRACTS - 1, len(abis))
+        assert_equal(EXPECTED_CONTRACTS, len(abis))
+        self.abis = abis
+
+    def match_abis(self):
+        self.abi_with_contract = []
+        candidates = {m[0]: m[1] for m in re.findall(r'\{super\(([$_\-\w]+?),"(\w+)"\)', self.script_data)}
+        for abi in self.abis:
+            assert_in(candidates, abi[0])
+            c = candidates[abi[0]]
+            assert_in(self.contracts, c)
+            self.abi_with_contract.append((abi[2], c))
+
+        assert_equal(EXPECTED_CONTRACTS, len(self.abi_with_contract))
+
+    def update_files(self):
+        path = CONTRACT_DIR
+        path.mkdir(exist_ok=True)
+        header = f'# generated automatically from {self.script_url} - DO NOT MODIFY'
+        init_lines = [header, '']
+        for (abi_definition, contract) in self.abi_with_contract:
+            camel = camel_to_snake(contract)
+            address = self.contracts[contract]
+            init_lines.append(f'from . import {camel}')
+            f = path / f'{camel}.py'
+            f.write_text(
+                f'''{header}
+
+CONTRACT = '{address}'
+
+ABI = {repr(abi_definition)}
+'''
+            )
+        (path / '__init__.py').write_text('\n'.join(init_lines))
+
+    def black_em(self):
+        subprocess.check_call(['black', CONTRACT_DIR])
 
     def run(self):
         self.fetch_script()
         self.extract_contracts()
         self.extract_abi()
+        self.match_abis()
+        # all contracts matched
+        assert_equal(set(), set(self.contracts.keys()) - set(x[1] for x in self.abi_with_contract))
+        self.update_files()
+        self.black_em()
 
 
 def main():
