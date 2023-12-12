04d1cd16bdeada929ef53906b1fd4cd1a5837086
Filipe Pina <hzlu1ot0@duck.com>
2023-12-12 00:07:10 +0000

    moved wallet persistence to its own files

--- cli/database.py
+++ cli/database.py
@@ -2,43 +2,11 @@ import json
 from pathlib import Path
 from typing import Optional
 
-from pydantic import BaseModel, Field, model_validator, AwareDatetime
+from pydantic import AwareDatetime, BaseModel, Field, model_validator
 
 
-class WalletDB(BaseModel):
-    slime_won: float = 0
-    notify_auto_claim: Optional[AwareDatetime] = None
-
-    global_db: 'GlobalDB' = Field(default=None, exclude=True)
-
-    def save(self) -> bool:
-        if self.global_db:
-            return self.global_db.save()
-        return False
-
-
-class GlobalDB(BaseModel):
-    wallets: dict[str, WalletDB] = {}
+class PersistMixin:
     save_file: Path = Field(default=None, exclude=True)
-
-    @model_validator(mode='after')
-    def add_db_to_wallets(self) -> 'GlobalDB':
-        for w in self.wallets.values():
-            w.global_db = self
-        return self
-
-    def add_wallet(self, owner):
-        if owner not in self.wallets:
-            self.wallets[owner] = WalletDB(global_db=self)
-        return self.wallets[owner]
-
-    def save(self, to: Path = None) -> bool:
-        if to is None:
-            to = self.save_file
-        if to is None:
-            return False
-        to.write_text(self.model_dump_json())
-        return True
 
     @classmethod
     def load_from_file(cls, filename: Path):
@@ -53,6 +21,41 @@ class GlobalDB(BaseModel):
             data = json.loads(raw_data)
         data['save_file'] = filename
         return cls(**data)
+
+    def save(self, to: Path = None) -> bool:
+        if to is None:
+            to = self.save_file
+        if to is None:
+            return False
+        to.write_text(self.model_dump_json())
+        return True
+
+
+class WalletDB(BaseModel, PersistMixin):
+    slime_won: float = 0
+    notify_auto_claim: Optional[AwareDatetime] = None
+
+    global_db: 'GlobalDB' = Field(default=None, exclude=True)
+
+
+class GlobalDB(BaseModel, PersistMixin):
+    wallets: dict[str, WalletDB] = Field(exclude=True, default={})
+    save_file: Path = Field(default=None, exclude=True)
+
+    @model_validator(mode='after')
+    def add_db_to_wallets(self) -> 'GlobalDB':
+        for k, w in self.wallets.items():
+            w.global_db = self
+            w.save_file = None if not self.save_file else self.save_file.with_stem(f'db-{k}')
+        return self
+
+    def add_wallet(self, owner):
+        if owner not in self.wallets:
+            if self.save_file:
+                self.wallets[owner] = WalletDB.load_from_file(self.save_file.with_stem(f'db-{owner}'))
+            else:
+                self.wallets[owner] = WalletDB(global_db=self)
+        return self.wallets[owner]
 
     def total_slime_won(self) -> float:
         total = 0.0
--- tests/test_database.py
+++ tests/test_database.py
@@ -8,9 +8,10 @@ from cli import database
 class Test(TestCase):
     def test_dump(self):
         db = database.GlobalDB()
-        db.add_wallet("x")
+        w = db.add_wallet("x")
         self.assertEqual(db.wallets['x'].global_db, db)
-        self.assertEqual(db.model_dump_json(), '{"wallets":{"x":{"slime_won":0.0}}}')
+        self.assertEqual(db.model_dump_json(), '{}')
+        self.assertEqual(w.model_dump_json(), '{"slime_won":0.0,"notify_auto_claim":null}')
 
     def test_load(self):
         db = database.GlobalDB(**{"wallets": {"x": {"slime_won": 1.5}}})
@@ -18,14 +19,25 @@ class Test(TestCase):
         self.assertEqual(db.wallets['x'].slime_won, 1.5)
 
     def test_persistence(self):
-        with tempfile.NamedTemporaryFile() as tmp_file:
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            tmp_path = Path(tmp_dir)
+            db_file = tmp_path / 'db.json'
+
             # load from non-existing file
-            db = database.GlobalDB.load_from_file(Path(tmp_file.name))
+            db = database.GlobalDB.load_from_file(db_file)
             self.assertEqual(db.wallets, {})
 
             wdb = db.add_wallet("x")
             wdb.slime_won = 2
-            db.save()
+            wdb.save()
 
-            another_db = database.GlobalDB.load_from_file(Path(tmp_file.name))
+            another_db = database.GlobalDB.load_from_file(db_file)
+            another_db.add_wallet('x')
             self.assertEqual(another_db.wallets['x'].slime_won, 2)
+
+    def test_defaults(self):
+        db = database.GlobalDB()
+        self.assertEqual(db.wallets, {})
+        db.wallets['x'] = 2
+        db2 = database.GlobalDB()
+        self.assertEqual(db2.wallets, {})
